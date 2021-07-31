fa1ba4853a6a5c148de5c4c42fd1fdc5968944c5
si3ver <izhouwl@163.com>
2021-08-01 03:48:48 +0800

    feat: main-app config

---
+++ practise/microfrontend/main/src/micro-app.js
@@ -0,0 +1,24 @@
+const microApps = [
+  {
+    name: 'sub-vue',
+    entry: process.env.VUE_APP_SUB_VUE,
+    activeRule: '/sub-vue',
+  },
+  {
+    name: 'sub-react',
+    entry: process.env.VUE_APP_SUB_REACT,
+    activeRule: '/sub-react',
+  },
+]
+
+const apps = microApps.map(item => {
+  return {
+    ...item,
+    container: '#subapp-viewport',
+    props: {
+      routerBase: item.activeRule,
+    }
+  }
+})
+
+export default apps
